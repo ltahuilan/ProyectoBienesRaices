b670462bd03e49d260d995e8a777bd3ca850a702
Luis Tahuilan <ltahuilan.ipn@gmail.com>
2021-08-28 23:04:13 -0500

    menu responsive & dark mode

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('Todo OK...');
+    eventsListeners();
+    darkMode();
 });
+
+function eventsListeners() {
+    const menu = document.querySelector('.mobil-menu');
+    menu.addEventListener('click', function() {
+        const navegacion = document.querySelector('.navegacion');
+
+        if(navegacion.classList.contains('mostrar')) {
+            navegacion.classList.remove('mostrar')
+        }else {
+            navegacion.classList.add('mostrar');
+        };
+
+        /**Modo corto con el método toggle */
+        // navegacion.classList.toggle('mostrar');
+    });
+};
+
+function darkMode() {
+    const darkMode = document.querySelector('.dark-mode-boton');
+
+    darkMode.addEventListener('click', function () {
+        document.body.classList.toggle('dark-mode');
+    });
+};
