c594944767c11ea8ffe71435dc00200cb2fd9c96
Luis Tahuilan <ltahuilan.ipn@gmail.com>
2021-08-29 08:55:00 -0500

    ajustes a Dark Mode para leer el tema configurado en el SO

--- src/js/app.js
+++ src/js/app.js
@@ -20,9 +20,49 @@ function eventsListeners() {
 };
 
 function darkMode() {
-    const darkMode = document.querySelector('.dark-mode-boton');
 
-    darkMode.addEventListener('click', function () {
-        document.body.classList.toggle('dark-mode');
+    /**acceder a las preferencias del sistema para detectar el tema configurado */
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // console.log(prefiereDarkMode.matches);
+
+    if(prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    }else{
+        document.body.classList.remove('dark-mode');
+    }
+
+    /**Escuchar los cambios de ajustes en el tema en las preferencias del sistema
+     *  para ajuste automático.
+     */
+    prefiereDarkMode.addEventListener('change', function() {
+        if(prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        }else{
+            document.body.classList.remove('dark-mode');
+        }
     });
+
+    
+    /**seleccion manual por usuario */
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+
+    botonDarkMode.addEventListener('click', function () {
+        document.body.classList.toggle('dark-mode');
+
+        /**almacenando las preferencias en localStorage 
+        * para leer en automatico si la página es cerrada
+        */
+       if(document.body.classList.contains('dark-mode')) {
+           localStorage.setItem('dark-mode-seleccionado', 'true');
+       }else{
+           localStorage.setItem('dark-mode-seleccionado', 'false');
+       }
+
+    });
+
+    /**Obtener las preferencias almacenadas en el local storge */
+    if(localStorage.getItem('dark-mode-seleccionado') === 'true') {
+        document.body.classList.add('dark-mode');
+    }
 };
